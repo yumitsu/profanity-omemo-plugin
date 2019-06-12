b2d6c4d916cbe220e05769f0e11f26b333253949
Alex Zinchenko <yumitsu@users.noreply.github.com>
2019-06-12 08:44:27 +0300

    Update installer.sh to support OSX

--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-which profanity > /dev/null
-which scanelf > /dev/null
-which tail > /dev/null
-which cut > /dev/null
-which tr > /dev/null
-which sed > /dev/null
+if [[ $OSTYPE == "darwin"* ]]; then
+    libtoolbin=$(command -v otool)
+    libtoolopts='-L'
+else
+    libtoolbin=$(command -v -- scanelf readelf greadelf | head -n1)
+    libtoolopts='-n'
+fi
 
-profanity="$(which profanity)"
-python_version="$(scanelf -n "${profanity}" | tail -n+2 | cut -d' ' -f2 | tr , '\n' | sed -nre 's/^libpython([0-9]+\.[0-9]+).*$/\1/p')"
+declare -r libtool="${libtoolbin}"
+declare -r sed=$(command -v -- gsed sed | head -n1)
+declare -r sedopts='-Ene'
+declare -r profanity="$(command -v profanity)"
+
+test -x "${libtoolbin}" || echo -n 'Couldnt find suitable binary, please be sure you have otool/readelf installed and accessible from current $PATH.'
+test -x "${sed}" || echo -n 'You should have sed/gsed installed and accessible from current $PATH.'
+test -x "${profanity}"
+
+command -v tail > /dev/null
+command -v cut > /dev/null
+command -v tr > /dev/null
+command -v sed > /dev/null
+
+python_version="$("${libtool}" "${libtoolopts}" "${profanity}" | tail -n+2 | cut -d' ' -f2 | tr , '\n' | ${sed} ${sedopts} 's/^libpython([0-9]+\.[0-9]+).*$/\1/p')"
 
 python"${python_version}" setup.py install --force --user --prefix=
 mkdir -p ~/.local/share/profanity/plugins
